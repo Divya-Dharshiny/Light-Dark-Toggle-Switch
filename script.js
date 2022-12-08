aa58065931662194b1a79a2a7049dff0a7c53120
Divya-Dharshiny <118870248+Divya-Dharshiny@users.noreply.github.com>
2022-12-08 11:41:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const mySpan = document.getElementById("mySpan");
+const myInput = document.getElementById("cbox");
+const navBtns = document.querySelectorAll(".nav-btn");
+console.log(navBtns);
+
+window.onload = () => {
+  mySpan.addEventListener("click", () => {
+    console.log(myInput.checked);
+    if (myInput.checked) {
+      document.body.style.background = "#FFF";
+      document.body.style.color = "#000";
+      for (let i=0; i < navBtns.length; i++) {
+        navBtns[i].style.color = "#000";
+        navBtns[i].addEventListener("mouseover", () => {
+          navBtns[i].style.borderColor = "#222";
+        });
+        navBtns[i].addEventListener("mouseout", () => {
+          navBtns[i].style.borderColor = "transparent";
+        });
+      }
+    } else {
+      document.body.style.background = "hsl(0, 1%, 8%)";
+      for (let i=0; i < navBtns.length; i++) {
+        navBtns[i].style.color = "#FFF";
+        navBtns[i].addEventListener("mouseover", () => {
+          navBtns[i].style.borderColor = "#eee";
+        });
+        navBtns[i].addEventListener("mouseout", () => {
+          navBtns[i].style.borderColor = "transparent";
+        });
+      }
+      document.body.style.color = "#FFF"; 
+    }
+  });
+}
